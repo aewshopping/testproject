3f901027bf612941747dfef7e3b5b615f7878a77
aewshopping <aewshopping@gmail.com>
2017-04-04 22:38:02 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,5 +6,5 @@ function setup() {
 function draw() {
 	noStroke();
 	fill(random(0,180),0,random(0,180),random(0,180));
-	 	ellipse(random(0,windowWidth),random(0,windowHeight),40,40);
+	ellipse(random(0,windowWidth),random(0,windowHeight),40,40);
 }
