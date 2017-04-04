ea1185e9872446877122d18a68cb310c8235b566
aewshopping <aewshopping@gmail.com>
2017-04-04 22:23:54 +0100

    Draw circles

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
+var xsize = windowWidth;
+var ysize = windowHeight;
+
 function setup() {
-  createCanvas(200,200);
+  createCanvas(xsize,ysize);
   background(0);
 }
 
 function draw() {
-  fill(255);
-  ellipse(100,100,40,40);
+  fill(random(0,180),0,random(0.180));
+  ellipse(random(0,xsize),random(0,ysize),40,40, random(50,180));
 }
