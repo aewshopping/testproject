5e16807540d2893b2e7992675754bf45fc979e47
aewshopping <aewshopping@gmail.com>
2017-04-04 22:39:44 +0100

    Debug change

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(400,400);
   background(0);
 }
 
 function draw() {
 	noStroke();
 	fill(random(0,180),0,random(0,180),random(0,180));
-	ellipse(random(0,windowWidth),random(0,windowHeight),40,40);
+	ellipse(random(0,400),random(0,400),40,40);
 }
