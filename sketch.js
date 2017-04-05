771d02b1c0b207c17ac85e04c4c942075ef173d5
aewshopping <aewshopping@gmail.com>
2017-04-05 10:29:30 +0100

    Change to full screen

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
-createCanvas(400,400);
+createCanvas(windowWidth,windowHeight);
 background(0);
 }
 
 function draw() {
 noStroke();
 fill(random(0,180),0,random(0,180),random(0,180));
-ellipse(random(0,400),random(0,400),40,40);
+ellipse(random(0,windowWidth),random(0,windowHeight),40,40);
 }
