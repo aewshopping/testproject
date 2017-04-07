429cfd96575a061e61d532fce4d8cf7cb74d80d1
aewshopping <aewshopping@gmail.com>
2017-04-07 13:51:41 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
+var s="Han Min"
+
 function setup() {
 createCanvas(windowWidth,windowHeight);
 background(0);
+textSize=150;
 }
 
 function draw() {
 noStroke();
 fill(random(0,120),0,random(0,180),random(0,180));
 ellipse(random(0,windowWidth),random(0,windowHeight),40,40);
+fill(0);
+text(s,50,100);
 }
